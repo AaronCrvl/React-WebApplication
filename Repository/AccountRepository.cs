761ec738f7877a278015768fc2b8bd33370fadd2
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2021-10-27 23:28:36 -0300

    Adding Context & Repository Classes

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Repository;
+
+namespace ReactWebApp.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly AppDbContext contexto;
+
+        public AccountRepository(AppDbContext _contexto)
+        {
+            contexto = _contexto;
+        }
+
+        public string Signin()
+        {
+            //Account acct =  contexto.Account.Find("TEST");
+            return new String("API CALEED SUCESS");
+        }
+    }
+}
